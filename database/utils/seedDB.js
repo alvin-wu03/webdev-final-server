7425595e55fef88c5ec5a44e5574cd876c320c70
xchen <xchen601@gmail.com>
2023-12-17 21:41:24 -0500

    fixed student seeds and added images

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -11,30 +11,36 @@ const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
 		address: "695 Park Ave, New York, NY 10065",
-		description: "This is a school in New York, New York."
+		description: "This is a school in New York, New York.",
+        imageURL: "../../images/hunter.jpg"
+
 	});
 	// Create a new campus
 	const dummy_campus2 = await Campus.create({
 		name: "Queens College",
 		address: "65-30 Kissena Blvd, Queens, NY 11367",
-		description: "This is a school in Queens, New York."
+		description: "This is a school in Queens, New York.",
+        imageURL: "../../images/queens.jpg"
 	});
 	// Create a new campus
 	const dummy_campus3 = await Campus.create({
 		name: "Brooklyn College",
 		address: "2900 Bedford Ave, Brooklyn, NY 11210",
-		description: "This is a school in Brooklyn, New York."
+		description: "This is a school in Brooklyn, New York.",
+        imageURL: "../../images/brooklyn.jpg"
 	});
 	
 	// Create a new student for a campus
 	const dummy_student = await Student.create({
 		firstname: "Joe",
       lastname: "Smith",
+      email: "joeeeeee@gmail.com"
 	});
 	// Create a new student for a campus
 	const dummy_student2 = await Student.create({
 		firstname: "Mary",
       lastname: "Johnson",
+      email: "maryjohnson@gmail.com"
 	});
 
 	// Add students to campuses
